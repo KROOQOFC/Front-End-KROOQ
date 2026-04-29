9b9255508732cd098eb81b830dc1e7e2eb6b5963
Arturqueiiroz <arturos.queiroz@gmail.com>
2026-04-28 21:20:46 -0300

    Deixando o login e o AuthLayout sem margin

--- krooq/src/components/AuthLayout/AuthLayout.jsx
+++ krooq/src/components/AuthLayout/AuthLayout.jsx
@@ -4,13 +4,15 @@ import bannerAuth from "../../assets/krooq_cadastre_se.png";
 function AuthLayout({ children }) {
   return (
     <main className="auth-layout">
-      <section className="auth-form-area">
-        {children}
-      </section>
+      <div className="auth-content">
+        <section className="auth-form-area">
+          {children}
+        </section>
 
-      <section className="auth-image-area">
-        <img src={bannerAuth} alt="Imagem da KROOQ" />
-      </section>
+        <section className="auth-image-area">
+          <img src={bannerAuth} alt="Imagem da KROOQ" />
+        </section>
+      </div>
     </main>
   );
 }
