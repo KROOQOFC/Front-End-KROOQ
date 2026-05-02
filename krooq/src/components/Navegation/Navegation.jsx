24894aac1a769b8aa9a0d7c45a7aa99e8aa0b84a
Alice Ferreira <alicefgalas@gmail.com>
2026-05-02 02:01:37 -0300

    Criação do footer. Aplicação de responsividade no footer e no menu de navegação.

--- krooq/src/components/Navegation/Navegation.jsx
+++ krooq/src/components/Navegation/Navegation.jsx
@@ -1,24 +1,35 @@
-import "./Navegation.css"
-import logoTipo from "../../assets/logoTipo.png"
-
+import { useState } from "react";
+import "./Navegation.css";
+import logoTipo from "../../assets/logoTipo.png";
 
 function Navegation() {
-    return (
-        <header>
-           
-            <nav className="conteiner">
-                 <img src= {logoTipo}/>
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Projetos</a></li>
-                    <li><a href="">Serviços</a></li>
-                    <li><a href="">Profissionais</a></li>
-                    <li><a href="">Jornada</a></li>
-                </ul>
-                 <button type="button" className="btn">Cadastrar</button>
-            </nav>
+  const [menuAberto, setMenuAberto] = useState(false);
 
-        </header>
-    )
+  return (
+    <header className="header">
+      <nav className="conteiner">
+
+        <img
+          src={logoTipo}
+          alt="Logo KROOQ"
+          className="logo"
+          onClick={() => setMenuAberto(!menuAberto)}
+        />
+
+        <ul className={menuAberto ? "menu ativo" : "menu"}>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Projetos</a></li>
+          <li><a href="#">Serviços</a></li>
+          <li><a href="#">Profissionais</a></li>
+          <li><a href="#">Jornada</a></li>
+          <li><button type="button" className="btn mobile-btn">Cadastrar</button></li>
+        </ul>
+
+        <button type="button" className="btn desktop-btn">Cadastrar</button>
+
+      </nav>
+    </header>
+  );
 }
-export default Navegation
+
+export default Navegation;
