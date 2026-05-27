018b153ccd2aeadabe612abeed00ec21c8b1a620
GabrielOliveira <gabrielcavalcantedeo@gmail.com>
2026-05-27 19:14:42 -0300

    componentes de metas e progresso da pagina de Profissionais

--- krooq/src/pages/CentralProfissional/CentralProfissional.jsx
+++ krooq/src/pages/CentralProfissional/CentralProfissional.jsx
@@ -1,23 +1,53 @@
-import "./CentralProfissional.css"
-import "../../components/NavegationLateral/NavegationLateral";
-import NavegationLateral from "../../components/NavegationLateral/NavegationLateral";
-import CentralHeader from "../../components/CentralHeader/CentralHeader"
+import "./CentralProfissional.css";
 
+import NavegationLateral from "../../components/NavegationLateral/NavegationLateral";
+import CentralHeader from "../../components/CentralHeader/CentralHeader";
+import MetasGerais from "../../components/MetasGerais/MetasGerais";
+import ProcessoCentral from "../../components/ProcessoCentral/ProcessoCentral";
+
+import SimboloProcesso from "../../assets/SimboloProcesso.png";
+import SimboloProcessoBranco from "../../assets/SimboloProcessoBranco.png";
+import ProcessoIconeComunidade from "../../assets/ProcessoIconeComunidade.png";
+import SimboloIconeExecutivo from "../../assets/SimboloIconeExecutivo.png";
 
 function CentralProfissional() {
-    return (  
-<section className="conteiner-central-profissional">
+  return (
+    <section className="conteiner-central-profissional">
+      <NavegationLateral />
 
-<NavegationLateral/>
-<div className="LayoutCentralHeader">
-<CentralHeader/>
-</div>
+      <CentralHeader />
 
+      <MetasGerais
+        titulo="Metas Gerais"
+        metas={[
+          { texto: "Especificação de Materiais", concluida: false },
+          { texto: "Detalhamento de Projeto", concluida: false },
+          { texto: "Modelagem 3D / Render", concluida: false },
+          { texto: "Entregas Prioritárias", concluida: false },
+        ]}
+      />
 
-</section>
+      <div className="AreaProcessos">
+        <ProcessoCentral
+  iconeTopo={ProcessoIconeComunidade}
+  titulo="Reunião com Engenheiro Civil"
+  responsavel="José Santos"
+  iconeStatus={SimboloProcessoBranco}
+  destaqueStatus={true}
+  onMenu={() => console.log("Abrir menu")}
+/>
 
-
-    );
+        <ProcessoCentral
+  iconeTopo={SimboloIconeExecutivo}
+  titulo="Apresentação de Conceito"
+  responsavel="Marília Teresa"
+  iconeStatus={SimboloProcesso}
+  destaqueStatus={false}
+  onMenu={() => console.log("Abrir menu")}
+/>
+      </div>
+    </section>
+  );
 }
 
 export default CentralProfissional;
