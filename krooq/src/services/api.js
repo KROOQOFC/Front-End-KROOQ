c2da4ffbe022eacabcff77ebf3aebf60f19cf3f2
Arturqueiiroz <arturos.queiroz@gmail.com>
2026-06-02 03:47:35 -0300

    Atualizar BASE_URL para usar variável de ambiente e remover código comentado

--- krooq/src/services/api.js
+++ krooq/src/services/api.js
@@ -1,4 +1,10 @@
-export const BASE_URL = "https://localhost:7062";
+// export const BASE_URL = "https://localhost:7062";
+
+// const API_URL = `${BASE_URL}/api`;
+
+// export default API_URL;
+
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 const API_URL = `${BASE_URL}/api`;
 
