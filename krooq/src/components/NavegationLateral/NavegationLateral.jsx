59fc03b99930219467c1d68b9d649fceb4066588
Arturqueiiroz <arturos.queiroz@gmail.com>
2026-06-02 02:52:54 -0300

    Refatorar NavegationLateral para melhorar a legibilidade e adicionar ícone de sair

--- krooq/src/components/NavegationLateral/NavegationLateral.jsx
+++ krooq/src/components/NavegationLateral/NavegationLateral.jsx
@@ -21,7 +21,6 @@ function NavegationLateral() {
       {/* MENU DESKTOP */}
       <aside className="nav-menu-lateral">
         <div className="layout-links-menu-lateral">
-
           <img
             className="logo-lateral"
             src={LogoLateral}
@@ -30,7 +29,6 @@ function NavegationLateral() {
 
           <nav>
             <ul>
-
               <li>
                 <NavLink to="/CentralProfissional" className="link-menu">
                   <img
@@ -38,9 +36,7 @@ function NavegationLateral() {
                     src={IconePainelGeral}
                     alt="Painel Geral"
                   />
-                  <span className="texto-painel">
-                    Painel Geral
-                  </span>
+                  <span className="texto-painel">Painel Geral</span>
                 </NavLink>
               </li>
 
@@ -53,10 +49,7 @@ function NavegationLateral() {
 
               <li>
                 <NavLink to="/tarefas" className="link-menu">
-                  <img
-                    src={IconeMinhasTarefas}
-                    alt="Minhas tarefas"
-                  />
+                  <img src={IconeMinhasTarefas} alt="Minhas tarefas" />
                   <span>Minhas Tarefas</span>
                 </NavLink>
               </li>
@@ -70,10 +63,7 @@ function NavegationLateral() {
 
               <li>
                 <NavLink to="/consultoria" className="link-menu">
-                  <img
-                    src={IconeConsultoria}
-                    alt="Consultoria"
-                  />
+                  <img src={IconeConsultoria} alt="Consultoria" />
                   <span>Consultoria</span>
                 </NavLink>
               </li>
@@ -85,122 +75,101 @@ function NavegationLateral() {
                     src={IconeKrooqIa}
                     alt="KROOQ IA"
                   />
-                  <span className="texto-krooq-ia-painel">
-                    KROOQ IA
-                  </span>
+                  <span className="texto-krooq-ia-painel">KROOQ IA</span>
                 </NavLink>
               </li>
 
+              {/* ÍCONE DE SAIR - DESKTOP */}
+              <li className="item-voltar-home">
+                <NavLink to="/home" className="link-menu">
+                  <FiLogOut className="icone-sair-menu" />
+                  <span>Voltar para o início</span>
+                </NavLink>
+              </li>
             </ul>
           </nav>
         </div>
       </aside>
 
       {/* MENU MOBILE */}
-     <aside className="menu-lateral-mobile">
+      <aside className="menu-lateral-mobile">
+        <div
+          className="container-lateral"
+          onClick={() => setMenuAberto(!menuAberto)}
+        >
+          <img
+            src={LogoKrooq}
+            alt="Logo Krooq"
+            className="logo-lateral-mobile"
+          />
+        </div>
 
-  <div
-    className="container-lateral"
-    onClick={() => setMenuAberto(!menuAberto)}
-  >
-    <img
-      src={LogoKrooq}
-      alt="Logo Krooq"
-      className="logo-lateral-mobile"
-    />
-  </div>
+        <ul className={menuAberto ? "menu-lateral ativo" : "menu-lateral"}>
+          <li>
+            <NavLink to="/CentralProfissional" onClick={() => setMenuAberto(false)}>
+              <img
+                className="icone-mobile-tamanho"
+                src={IconePainelGeral}
+                alt="Painel Geral"
+              />
+              <span>Painel Geral</span>
+            </NavLink>
+          </li>
 
-  <ul className={menuAberto ? "menu-lateral ativo" : "menu-lateral"}>
+          <li>
+            <NavLink to="/agenda" onClick={() => setMenuAberto(false)}>
+              <img className="icone-mobile" src={IconeAgenda} alt="Agenda" />
+              <span>Agenda</span>
+            </NavLink>
+          </li>
 
-    <li>
-      <NavLink to="/CentralProfissional">
-        <img
-          className="icone-mobile-tamanho"
-          src={IconePainelGeral}
-          alt="Painel Geral"
-        />
-        <span>Painel Geral</span>
-      </NavLink>
-    </li>
+          <li>
+            <NavLink to="/tarefas" onClick={() => setMenuAberto(false)}>
+              <img
+                className="icone-mobile"
+                src={IconeMinhasTarefas}
+                alt="Minhas Tarefas"
+              />
+              <span>Minhas Tarefas</span>
+            </NavLink>
+          </li>
 
-    <li>
-      <NavLink to="/agenda">
-        <img
-          className="icone-mobile"
-          src={IconeAgenda}
-          alt="Agenda"
-        />
-        <span>Agenda</span>
-      </NavLink>
-    </li>
+          <li>
+            <NavLink to="/projetos" onClick={() => setMenuAberto(false)}>
+              <img className="icone-mobile" src={IconeProjetos} alt="Projetos" />
+              <span>Projetos</span>
+            </NavLink>
+          </li>
 
-    <li>
-      <NavLink to="/tarefas">
-        <img
-          className="icone-mobile"
-          src={IconeMinhasTarefas}
-          alt="Minhas Tarefas"
-        />
-        <span>Minhas Tarefas</span>
-      </NavLink>
-    </li>
+          <li>
+            <NavLink to="/consultoria" onClick={() => setMenuAberto(false)}>
+              <img className="icone-mobile" src={IconeConsultoria} alt="Consultoria" />
+              <span>Consultoria</span>
+            </NavLink>
+          </li>
 
-    <li>
-      <NavLink to="/projetos">
-        <img
-          className="icone-mobile"
-          src={IconeProjetos}
-          alt="Projetos"
-        />
-        <span>Projetos</span>
-      </NavLink>
-    </li>
+          <li className="krooqia-alinhar">
+            <NavLink to="/krooqia" onClick={() => setMenuAberto(false)}>
+              <img
+                className="icone-mobile-tamanho-krooqia"
+                src={IconeKrooqIa}
+                alt="KROOQ IA"
+              />
+              <span className="texto-krooqai-responsive">KROOQ IA</span>
+            </NavLink>
+          </li>
 
-    <li>
-      <NavLink to="/consultoria">
-        <img
-          className="icone-mobile"
-          src={IconeConsultoria}
-          alt="Consultoria"
-        />
-        <span>Consultoria</span>
-      </NavLink>
-    </li>
-
-    <li className="krooqia-alinhar">
-      <NavLink to="/krooqia">
-        <img
-          className="icone-mobile-tamanho-krooqia"
-          src={IconeKrooqIa}
-          alt="KROOQ IA"
-        />
-        <span className="texto-krooqai-responsive">KROOQ IA</span>
-      </NavLink>
-    </li>
-  </ul>
-</aside>
+          {/* ÍCONE DE SAIR - MOBILE */}
+          <li className="item-voltar-home">
+            <NavLink to="/home" onClick={() => setMenuAberto(false)}>
+              <FiLogOut className="icone-mobile" />
+              <span>Voltar para o início</span>
+            </NavLink>
+          </li>
+        </ul>
+      </aside>
     </>
   );
-                        <li>
-                            <NavLink to="/krooqia" className="link-menu">
-                                <img className="imagem-krooq-ia"
-                                    src={IconeKrooqIa}
-                                    alt="KROOQ IA"/>
-                                    
-                                <span className="texto-krooq-ia-painel">KROOQ IA</span>
-                            </NavLink>
-                        </li>
-                        <li className="item-voltar-home">
-                        <NavLink to="/home" className="link-menu">
-                            <FiLogOut className="icone-sair-menu" />
-                            <span>Voltar para o início</span>
-                        </NavLink>
-                        </li>                
-                    </ul>
-                </nav>
-            </div>
-        </aside>
-    );
 }
 
 export default NavegationLateral;
